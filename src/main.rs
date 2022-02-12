0678352c4a5f8cd27fea6b8c0ba0d2e8c6492c7a
Austin Johnson <me@austinj.work>
2022-02-12 02:03:59 -0600

    working?

---
+++ src/main.rs
@@ -0,0 +1,117 @@
+use std::sync::Arc;
+
+fn main() {
+    let buffer = Arc::new(Buffer { });
+    let another_buffer = Arc::new(AnotherBuffer { });
+ 
+    takes_collection(buffer.clone());
+    takes_collection(vec![buffer.clone(), buffer.clone()]);
+    takes_collection((buffer.clone(), buffer.clone()));
+
+    let buffer_trait_obj = buffer.clone() as Arc<dyn BufferAccess>;
+    let another_buffer_trait_obj = another_buffer.clone() as Arc<dyn BufferAccess>;
+
+    takes_collection(buffer_trait_obj.clone());
+    takes_collection(vec![buffer_trait_obj.clone(), another_buffer.clone()]);
+    takes_collection((buffer_trait_obj.clone(), another_buffer_trait_obj.clone()));
+    takes_collection((buffer_trait_obj.clone(), another_buffer.clone()));
+}
+
+pub struct Buffer { }
+pub struct AnotherBuffer { }
+
+pub trait BufferAccess { }
+impl BufferAccess for Buffer { }
+impl BufferAccess for Arc<Buffer> { }
+impl BufferAccess for AnotherBuffer { }
+impl BufferAccess for Arc<AnotherBuffer> { }
+
+pub trait BufferAccessObject {
+    fn into_object(&self) -> Arc<dyn BufferAccess>;
+}
+
+impl BufferAccessObject for Arc<dyn BufferAccess> {
+    fn into_object(&self) -> Arc<dyn BufferAccess> {
+        self.clone()
+    }
+}
+
+impl BufferAccessObject for Arc<Buffer> {
+    fn into_object(&self) -> Arc<dyn BufferAccess> {
+        self.clone()
+    }
+}
+
+impl BufferAccessObject for Arc<AnotherBuffer> {
+    fn into_object(&self) -> Arc<dyn BufferAccess> {
+        self.clone()
+    }
+}
+
+pub trait BufferCollection {
+    fn into_vec(self) -> Vec<Arc<dyn BufferAccess>>;
+}
+
+impl BufferCollection for () {
+    fn into_vec(self) -> Vec<Arc<dyn BufferAccess>> {
+        Vec::new()
+    }
+}
+
+impl<T: BufferAccessObject> BufferCollection for T {
+    fn into_vec(self) -> Vec<Arc<dyn BufferAccess>> {
+        vec![self.into_object()]
+    }
+}
+
+impl<T: BufferAccessObject> BufferCollection for Vec<T> {
+    fn into_vec(self) -> Vec<Arc<dyn BufferAccess>> {
+        self.into_iter().map(|src| src.into_object()).collect()
+    }
+}
+
+impl BufferCollection for Vec<Arc<dyn BufferAccessObject>> {
+    fn into_vec(self) -> Vec<Arc<dyn BufferAccess>> {
+        self.into_iter().map(|src| src.into_object()).collect()
+    }
+}
+
+/*impl<A: BufferAccessObject, B: BufferAccessObject> BufferCollection for (A, B) {
+    fn into_vec(self) -> Vec<Arc<dyn BufferAccess>> {
+        vec![self.0.into_object(), self.1.into_object()]
+    }
+}*/
+
+macro_rules! impl_collection {
+    ($first:ident $(, $others:ident)+) => (
+        impl<$first$(, $others)+> BufferCollection for ($first, $($others),+)
+            where $first: BufferAccessObject
+                  $(, $others: BufferAccessObject)*
+        {
+            #[inline]
+            fn into_vec(self) -> Vec<Arc<dyn BufferAccess>> {
+                #![allow(non_snake_case)]
+
+                let ($first, $($others,)*) = self;
+                let mut list = Vec::new();
+                list.push($first.into_object());
+
+                $(
+                    list.push($others.into_object());
+                )+
+
+                list
+            }
+        }
+
+        impl_collection!($($others),+);
+    );
+
+    ($i:ident) => ();
+}
+
+impl_collection!(Z, Y, X, W, V, U, T, S, R, Q, P, O, N, M, L, K, J, I, H, G, F, E, D, C, B, A);
+
+fn takes_collection<C: BufferCollection>(collection: C) {
+    let _vec: Vec<Arc<dyn BufferAccess>> = collection.into_vec();
+}
